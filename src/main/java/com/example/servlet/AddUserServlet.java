f2a81fadb17eecba67bdd35fb4022b3eb477d99d
Hanna_Urban <huan92@yandex.ru>
2024-03-26 01:11:22 +0300

    done

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -5,12 +5,13 @@ import com.example.Warehouse;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Set;
-
+@WebServlet("/jsp/add")
 public class AddUserServlet extends HttpServlet {
 
     @Override
